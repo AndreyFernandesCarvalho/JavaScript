214fd7ba6328456d242928db6080508273ee14e9
Andrey Fernandes Carvalho <andreyfernandescarvalho@gmail.com>
2025-12-19 19:38:51 -0300

    Create script.js

---
+++ exercicios/aula15/script.js
@@ -0,0 +1,7 @@
+let cidade = ["Cachoeiro de Itapemirim", "Vila Velha", "Serra", "Cariacica", "Guarapari"];
+
+if (cidade.indexOf("Cachoeiro de Itapemirim") != -1) {
+    console.log("A cidade foi encontrada");
+} else {
+    console.log("A cidade não foi encontrada");
+}
