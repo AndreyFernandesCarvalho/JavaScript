eef3b45c55316517beba7b6554ec325e07f397a0
Andrey Fernandes Carvalho <andreyfernandescarvalho@gmail.com>
2025-12-26 12:38:15 -0300

    contador de clicks

---
+++ exercicios/contador-de-clicks/script.js
@@ -0,0 +1,18 @@
+let num = document.querySelector("#num");
+let btnAcrescentar = document.querySelector("#btn-acrescentar");
+let contador = 0;
+
+function acrescentar() {
+    contador++;
+    num.innerHTML = contador;
+}
+
+function decrementar() {
+    contador--;
+    num.innerHTML = contador;
+}
+
+function reset() {
+    contador = 0;
+    num.innerHTML = contador;
+}
