aee0952b7164f1c913d2f41167743f00ef2c75e0
Andrey Fernandes Carvalho <andreyfernandescarvalho@gmail.com>
2026-01-01 13:58:08 -0300

    Update desafio.js

--- exercicios/objetos/desafio.js
+++ exercicios/objetos/desafio.js
@@ -1,16 +1,26 @@
-const nome = document.querySelector("#nome");
-const sobrenome = document.querySelector("#sobrenome");
-const idade = document.querySelector("#idade");
-const altura = document.querySelector("#altura");
-const respostas = document.querySelector("#respostas");
 const pessoas = [];
 
-function enviar() {
+function enviar(event) {
+    const nome = document.querySelector("#nome").value;
+    const sobrenome = document.querySelector("#sobrenome").value;
+    const idade = document.querySelector("#idade").value;
+    const altura = document.querySelector("#altura").value;
+    const respostas = document.querySelector("#respostas").value;
+
     if (nome.value == "" || sobrenome.value == "" || idade.value == "" || altura.value == "") {
         alert("Por favor, preencha os dados!");
     } else {
-        respostas.innerHTML = `${nome.value} ${sobrenome.value} ${idade.value} ${altura.value}`;
+        
+        const pessoa = {
+            nome,
+            sobrenome,
+            idade,
+            altura
+        };
+
+        pessoas.push(pessoa);
+        console.log(pessoas);
+
     } 
-    
 }
 
